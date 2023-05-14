bca9da0ad9aeac182a0873b5629f35f1c4a157d2
Deep Lekhak <lekhakdeepak10@gmail.com>
2023-05-14 03:54:29 -0400

    some major changes, would be f'up without git

--- background.js
+++ background.js
@@ -1,62 +1,51 @@
-// var blockedSites = [];
-
+// global vars
 const blockedSites = JSON.parse(localStorage.getItem('blockedSites') || '[]');
-
-const siteInput = document.querySelector('.site-title');
-const addButton = document.querySelector('.add-btn');
 var addbox = document.querySelector('.add-site');
 let index = 0;
+let index1 = 0;
+const addButton = document.getElementById("add-btn-id");
+const removeButton = document.getElementById('remove-btn');
 
 
-const addButton_e = document.getElementById("add-btn-id");
-const removeButton = document.querySelector('remove-btn');
-
-if (addButton_e) {
-  addButton_e.addEventListener('click', () => {
-    addSite();
-  });
-}
-
-if (removeButton) {
-  addButton.addEventListener('click', () => {
-    removeSite();
-  });
-}
 
 // DO NOT EDIT THIS CODE BLOCK
-if (chrome.webRequest) {
-chrome.webRequest.onBeforeRequest.addListener(
-  function(details) {
-    // Check if the request URL matches any of the blocked sites
-    for (let i = 0; i < blockedSites.length; i++) {
-      if (details.url.includes(blockedSites[i])) {
-        console.log("Blocked request:", details.url);
-        return {cancel: true}; // Cancel the request
+function blockSite() {
+  if (chrome.webRequest) {
+  chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+      // Check if the request URL matches any of the blocked sites
+      for (let i = 0; i < blockedSites.length; i++) {
+        if (details.url.includes(blockedSites[i])) {
+          console.log("Blocked request:", details.url);
+          return {cancel: true}; // Cancel the request
+        }
       }
-    }
-  },
-  {urls: ["<all_urls>"]},
-  ["blocking"]
-);
+    },
+    {urls: ["<all_urls>"]},
+    ["blocking"]
+  );
+  }
 }
 // END OF DONOT EDIT
 
 
-
+// shows all the sites that are currently on the server list
 function showAllSites() {
   for (let i = 0; i < blockedSites.length; i++) {
     siteName = blockedSites[i];
-    let divEl = `<div id="site-block">
-    <text id="site-text"> ${siteName}</text> 
-      <label class="switch">
-        <input type="checkbox">
-        <span class="slider round"></span>a
-      </label>
+    let divEl = `<div class="site-block">
+        <text id="site-text"> ${siteName}</text> 
+        <label class="switch">
+            <input type="checkbox" id="toggle-${i}">
+            <span class="slider round"></span>
+        </label>
     </div>`;
   addbox.insertAdjacentHTML('afterend', divEl);
   }
 }
 showAllSites();
+
+
 // purpose is to reduce duplication of values
 function checkSiteName(siteName) {
   for (let i = 0; i < blockedSites.length; i++) {
@@ -68,17 +57,19 @@ function checkSiteName(siteName) {
   return false;
 }
 
-function showSite(site) {
-    let divEl = `<div id="site-block">
-    <text id="site-text"> ${site}</text> 
-      <label class="switch">
-        <input type="checkbox">
-        <span class="slider round"></span>a
-      </label>
-    </div>`;
-  addbox.insertAdjacentHTML('afterend', divEl);
 
+// shows a particular site
+function showSite(site, index) {
+  let divEl = `<div class="site-block">
+                <text id="site-text"> ${site}</text> 
+                <label class="switch">
+                    <input type="checkbox" id="toggle-${index}">
+                    <span class="slider round"></span>
+                </label>
+              </div>`;
+  addbox.insertAdjacentHTML('afterend', divEl);
 }
+
 
 // purpose is to add site to list of blocked sites
 function addSite() {
@@ -92,7 +83,8 @@ function addSite() {
   }
   if (checkSiteName(siteName) == false) {
     blockedSites.push(siteName);
-    showSite(siteName);
+    showSite(siteName, index1);
+    index1++;
   }
   else {
     window.alert("Site already exits!")
@@ -120,3 +112,45 @@ function removeSite () {
   location.reload();
 }
 
+function toggleStat(toggle) {
+  if (document.getElementById('toggle')) {
+    toggle.addEventListener('change', (event) => {
+      if (event.target.checked) {
+        return true;
+      } else {
+        return false;
+      }
+    });  
+  }
+  else {
+    return;
+  }
+}
+
+
+if (addButton) {
+  addButton.addEventListener('click', () => {
+    addSite();
+  });
+}
+
+if (removeButton) {
+  removeButton.addEventListener('click', () => {
+    removeSite();
+  });
+}
+
+
+// main game loop
+
+for (let i = 0; i < blockedSites.length;i++) {
+    toggle_id = "toggle-" + i;
+    let toggle = document.getElementById(toggle_id);
+    if (toggle) {
+      state  = toggleStat(toggle);
+      if (state == false) {
+        site_text = document.getElementById("site-text");
+        console.log(site_text);
+      }
+    }
+}
