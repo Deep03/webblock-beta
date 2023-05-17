fb47bf56b83041c574fb91b4aaea108b9f55a92e
Deep Lekhak <lekhakdeepak10@gmail.com>
2023-05-17 02:00:21 -0400

    some thing

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 // global vars
 const blockedSites = JSON.parse(localStorage.getItem('blockedSites') || '[]');
-const current = JSON.parse(localStorage.getItem('current') || '[]');
-localStorage.setItem('current', JSON.stringify(current));
-
 var addbox = document.querySelector('.add-site');
 let index = 0;
 let site_counter = 0;
 const addButton = document.getElementById("add-btn-id");
 const removeButton = document.getElementById('remove-btn');
 let remove_index = 0;
+let skip_list = [];
 
-
-
+for (let i = 0; i < blockedSites.length; i++) {
+    skip_list.push(i);
+}
 
 // DO NOT EDIT THIS CODE BLOCK
 function blockSite() {
@@ -32,8 +31,8 @@ function blockSite() {
 
 // shows all the sites that are currently on the server list
 function showAllSites() {
-  for (let i = 0; i < current.length; i++) {
-    siteName = current[i];
+  for (let i = 0; i < blockedSites.length; i++) {
+    siteName = blockedSites[i];
     let divEl = `<div class="site-block">
         <text id="site-text"> ${siteName}</text> 
         <label class="switch">
@@ -49,8 +48,8 @@ showAllSites();
 
 // purpose is to reduce duplication of values
 function checkSiteName(siteName) {
-  for (let i = 0; i < current.length; i++) {
-    if (current[i] === siteName) {
+  for (let i = 0; i < blockedSites.length; i++) {
+    if (blockedSites[i] === siteName) {
       index = i;
       return true;
     }
@@ -84,7 +83,6 @@ function addSite() {
   }
   if (checkSiteName(siteName) == false) {
     blockedSites.push(siteName);
-    current.push(siteName);
     showSite(siteName, site_counter);
     site_counter++;
   }
@@ -92,25 +90,26 @@ function addSite() {
     window.alert("Site already exits!")
   }
   localStorage.setItem('blockedSites', JSON.stringify(blockedSites));
-  localStorage.setItem('current', JSON.stringify(current));
+  blockSite();
 }
 
+console.log(blockedSites);
 // purpose is to make it available to remove sites
 function removeSite () {
   siteName = document.querySelector('.site-title').value;
   document.querySelector('.site-title').value = '';
-  if (current.length == 0) {
+  if (blockedSites.length == 0) {
     window.alert("Site does not exist");
     return;
   }
   if (checkSiteName(siteName) == true) {
-    current.splice(index, 1);
+    blockedSites.splice(index, 1);
   }
   else {
     window.alert("Site does not exist");
     return;
   }
-  localStorage.setItem('current', JSON.stringify(current));
+  localStorage.setItem('blockedSites', JSON.stringify(blockedSites));
   location.reload();
 }
 
@@ -128,6 +127,21 @@ if (removeButton) {
   });
 }
 
-console.log(blockedSites);
 
-console.log(current + " Current Sites");
+// main game loop
+for (let i = 0; i < blockedSites.length;i++) {
+    let toggle_id = "toggle-" + i;
+    let toggle = document.getElementById(toggle_id);
+    toggle.addEventListener('change', (event) => {
+      if (!event.target.checked) {
+        skip_list.push(i);
+      } else {
+        var index = skip_list.indexOf(i);
+        if (index > -1) {
+          skip_list.splice(index, 1);
+        }
+        // Perform actions when checkbox is unchecked
+      }
+      console.log(skip_list);
+    });
+}
